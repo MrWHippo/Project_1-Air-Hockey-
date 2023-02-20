d8b1b17f504061eefc1404acb502e7b9c820ef6e
Jonty <17radcj@olab.org.uk>
2023-02-20 20:08:21 +0000

    Unfinished V0.03

--- AirHockey.py
+++ AirHockey.py
@@ -1,14 +1,25 @@
 from kivy.app import App 
 from kivy.uix.widget import Widget
+from kivy.clock import Clock
 
 
 class HockeyGame(Widget):
-    pass
+
+    def update(self, dt):
+        
+        def on_touch_move(self, touch):
+            if touch.y < self.hieght/2:
+                print("High")
+            elif touch.y > self.hieght/2:
+                print("Low")
+        pass
 
 
 class HockeyApp(App):
     def build(self):
-        return HockeyGame()
+        Game= HockeyGame()
+        Clock.schedule_interval(Game.update, 1.0 / 60.0)
+        return Game
 
 if __name__ == '__main__':
     HockeyApp().run()
