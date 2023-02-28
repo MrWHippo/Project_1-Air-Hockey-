5a086488dcea00d1f39a8edbad5fd38c25c2ba8c
Jonty Radcliffe <17radcj@olab.org.uk>
2023-02-28 16:00:57 +0000

    V0.03 Unfinished

--- AirHockey.py
+++ AirHockey.py
@@ -1,22 +1,27 @@
 from kivy.app import App 
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 from kivy.clock import Clock
 
-
+#parent widget!!
 class DifficultyScreen(Widget):
+    def update(self, dt):
+        pass
+    def CheckClick(self, num):
+        pass
+
+class HockeyGame(Widget):
 
     def update(self, dt):
         pass
 
-class HockeyGame(Widget):
-    pass
 
 class HockeyApp(App):
     def build(self):
         Game= HockeyGame()
-        return Game
         #Clock.schedule_interval(Game.update, 1.0 / 60.0)
         #return DifficultyScreen()
-
+        return Game
+    
 if __name__ == '__main__':
     HockeyApp().run()
