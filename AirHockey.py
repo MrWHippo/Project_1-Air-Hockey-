03b4edaf4731589f915f3a6bbd65b3fbe5ca8a0f
Jonty <17radcj@olab.org.uk>
2023-03-02 22:05:51 +0000

    V0.4 finished

--- AirHockey.py
+++ AirHockey.py
@@ -7,29 +7,31 @@ class DifficultyScreen(Widget):
     def update(self, dt):
         pass
     def CheckClick(self, num):
-        pass
-
-class HockeyGame(Widget):
-
-    def update(self, dt):
+        print(num)
+        self.parent.changescreen(num)
         pass
 
 class HockeyRoot(FloatLayout):
     def __init__(self, **kwargs):
         super(HockeyRoot, self).__init__(**kwargs)
         self.add_widget(DifficultyScreen())
-        #self.add_widget(HockeyGame())
+    
+    def changescreen(self, num):
+        self.clear_widgets()
+        self.add_widget(HockeyGame())
+    
+    def update(self, dt):
+        pass
 
+class HockeyGame(Widget):
     def update(self, dt):
         pass
 
 
 class HockeyApp(App):
     def build(self):
-        #Game = HockeyGame()
-        #Menu = DifficultyScreen()
-        #Clock.schedule_interval(Game.update, 1.0 / 60.0)
         Game = HockeyRoot()
+        Clock.schedule_interval(Game.update, 1.0 / 60.0)
         return Game
     
 if __name__ == '__main__':
